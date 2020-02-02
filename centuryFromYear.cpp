42f26675392d2dd8013ada4754e07d823e12ef9c
HEMANT KUMAR <hkbiet@gmail.com>
2020-02-02 19:47:51 +0530

    Solved 3 problems

---
+++ centuryFromYear.cpp
@@ -0,0 +1,6 @@
+int centuryFromYear(int year) {
+
+	return ((year-1)/100 + 1);
+
+    }
+
---
+++ checkPalindrome.cpp
@@ -0,0 +1,34 @@
+#include <cstring>
+bool checkPalindrome(std::string inputString) {
+
+    int code = 0;
+    int front = 0;
+    int end = strlen(inputString.c_str());
+    end = end -1 ;
+ 
+    while(front != strlen(inputString.c_str())){
+
+        if(inputString[front] != inputString[end]){
+
+            code = 1;
+            cout<<"break"<<endl;
+            break;
+        }
+
+        front++;
+        end--;
+
+    }
+
+    if(!code){
+        
+        return true;
+
+    }else {
+
+
+        return false;
+    }
+
+}
+
