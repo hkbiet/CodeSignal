22546f48385a4ab688450ca73fe1aaa4c08cb81c
HEMANT KUMAR <hkbiet@gmail.com>
2020-02-04 16:35:58 +0530

    new ones

---
+++ almostIncreasingSequence.cpp
@@ -0,0 +1,34 @@
+bool almostIncreasingSequence(std::vector<int> sequence) {
+/*
+if(is_sorted(sequence.begin(), sequence.end())){
+        return true;
+        }
+  */  
+    int max = INT_MIN;
+    int secondMax  = INT_MIN;
+    int count = 0;
+    int i = 0;
+
+    while(i < sequence.size()){
+        if(sequence[i] > max){
+            cout<<"normal seq seq["<<i<<"] > max("<<max<<")"<<endl;
+            secondMax = max;
+            max = sequence[i];
+            cout<<" --normal.. after swaps secondmax = "<<secondMax<<" max ="<<sequence[i]<<endl;
+            
+        }else if(sequence[i] > secondMax){
+            cout<<"seq["<<i<<"] >"<<secondMax<<endl;
+            max = sequence[i];
+            count++;
+            cout<<"count after increase = "<<count<<endl;
+        }else {count++; cout<<"ELSE count++ = "<<count<<endl;}
+
+
+        i++;
+    }
+   
+   return count <= 1;
+
+
+}
+
---
+++ arrayMakeConsecutive.cpp
@@ -0,0 +1,27 @@
+int makeArrayConsecutive2(std::vector<int> statues) {
+
+    sort(statues.begin(), statues.end());
+    for(auto x: statues)
+        cout<<x<<"\t";
+    int size = statues.size();
+    int i = 0;
+    int total = 0;
+    int diff = 0;
+
+    while(i < size-1){
+
+        diff = statues[i+1] - statues[i];
+        cout<<"diff = "<<diff<<endl;
+        if(diff > 1){
+
+            total = total + diff - 1;
+            diff = 0;
+        }
+        diff = 0;
+
+        i++;
+    }
+
+    return total;
+}
+
